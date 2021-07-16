430580fc4c3553cbec1d9ead89a0ec8d4d94e588
Gui Vieira <guialbuk@gmail.com>
2021-07-16 12:18:29 -0700

    Deliver segment files (#2)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod database;
 mod models;
 mod schema;
 
@@ -7,37 +8,71 @@ extern crate diesel;
 #[macro_use]
 extern crate rocket;
 
-use diesel::pg::PgConnection;
-use diesel::prelude::*;
-use diesel::Connection;
-use dotenv::dotenv;
 use models::*;
+use regex::Regex;
 use rocket::fs::NamedFile;
-use std::env;
+use rocket::http::ContentType;
+use rocket::response::content::Custom;
+use rocket::response::status::NotFound;
 use std::path::Path;
 
-pub fn establish_connection() -> PgConnection {
-    dotenv().ok();
+#[get("/segments/<file_name>")]
+async fn segment(file_name: &str) -> Result<Custom<NamedFile>, NotFound<String>> {
+    let regex = Regex::new(r"([a-zA-Z0-9]{10})_(\d+)\.ts").unwrap();
+    let capture_groups = regex.captures(file_name).unwrap();
 
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let variant_pid = capture_groups.get(1).map_or("", |m| m.as_str());
+    let segment_position = capture_groups.get(2).map_or("", |m| m.as_str());
 
-    PgConnection::establish(&database_url).expect("Error connecting to the database.")
-}
+    let variants = Variant::by_public_id(variant_pid);
 
-#[get("/")]
-async fn index() -> Option<NamedFile> {
-    use crate::schema::videos::dsl::*;
+    let first_variant = variants.first();
 
-    let connection = establish_connection();
+    let variant = match first_variant {
+        Some(variant) => variant,
+        None => return Result::Err(NotFound("Variant not found.".to_string())),
+    };
 
-    let results = videos.limit(5).load::<Video>(&connection);
+    let segments = Segment::by_position(variant.id, segment_position.parse::<i32>().unwrap());
 
-    println!("{:?}", results);
+    let first_segment = segments.first();
 
-    NamedFile::open(Path::new("./storage/test.txt")).await.ok()
+    let segment = match first_segment {
+        Some(segment) => segment,
+        None => return Result::Err(NotFound("Segment not found.".to_string())),
+    };
+
+    let attachment = ActiveStorageAttachment::by_segment(segment.id);
+
+    let first_attachment = attachment.first();
+
+    let attachment = match first_attachment {
+        Some(attachment) => attachment,
+        None => return Result::Err(NotFound("Attachment not found.".to_string())),
+    };
+
+    let blob = ActiveStorageBlob::find_by_id(attachment.blob_id);
+
+    let key = blob.key;
+
+    let path = format!(
+        "/Users/gui/dev/albuvideo-api/storage/{}/{}/{}",
+        &key[..=1],
+        &key[2..=3],
+        key
+    );
+
+    let named_file = NamedFile::open(Path::new(&path)).await;
+
+    let content_type = ContentType::new("video", "x-MP2T");
+
+    match named_file {
+        Ok(named_file) => Result::Ok(Custom(content_type, named_file)),
+        Err(e) => Result::Err(NotFound(e.to_string())),
+    }
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![segment])
 }
--- src/schema.rs
+++ src/schema.rs
@@ -1,4 +1,122 @@
 table! {
+    access_tokens (id) {
+        id -> Int8,
+        name -> Varchar,
+        access_token -> Varchar,
+        public_id -> Varchar,
+        user_id -> Int8,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
+
+table! {
+    active_storage_attachments (id) {
+        id -> Int8,
+        name -> Varchar,
+        record_type -> Varchar,
+        record_id -> Int8,
+        blob_id -> Int8,
+        created_at -> Timestamp,
+    }
+}
+
+table! {
+    active_storage_blobs (id) {
+        id -> Int8,
+        key -> Varchar,
+        filename -> Varchar,
+        content_type -> Nullable<Varchar>,
+        metadata -> Nullable<Text>,
+        service_name -> Varchar,
+        byte_size -> Int8,
+        checksum -> Varchar,
+        created_at -> Timestamp,
+    }
+}
+
+table! {
+    active_storage_variant_records (id) {
+        id -> Int8,
+        blob_id -> Int8,
+        variation_digest -> Varchar,
+    }
+}
+
+table! {
+    ar_internal_metadata (key) {
+        key -> Varchar,
+        value -> Nullable<Varchar>,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
+
+table! {
+    schema_migrations (version) {
+        version -> Varchar,
+    }
+}
+
+table! {
+    segments (id) {
+        id -> Int8,
+        variant_id -> Int8,
+        position -> Int4,
+        duration -> Float8,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
+
+table! {
+    signature_keys (id) {
+        id -> Int8,
+        name -> Varchar,
+        signature_key -> Varchar,
+        public_id -> Varchar,
+        user_id -> Int8,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
+
+table! {
+    users (id) {
+        id -> Int8,
+        email -> Varchar,
+        encrypted_password -> Varchar,
+        remember_created_at -> Nullable<Timestamp>,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
+
+table! {
+    variants (id) {
+        id -> Int8,
+        video_id -> Int8,
+        public_id -> Varchar,
+        height -> Int4,
+        width -> Int4,
+        bitrate -> Int4,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
+
+table! {
+    video_watch_events (id) {
+        id -> Int8,
+        video_id -> Int8,
+        user_id -> Int8,
+        duration -> Float8,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
+
+table! {
     videos (id) {
         id -> Int8,
         title -> Nullable<Varchar>,
@@ -11,3 +129,42 @@ table! {
         user_id -> Int8,
     }
 }
+
+table! {
+    webhooks (id) {
+        id -> Int8,
+        topic -> Varchar,
+        url -> Varchar,
+        user_id -> Int8,
+        public_id -> Nullable<Varchar>,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
+
+joinable!(access_tokens -> users (user_id));
+joinable!(active_storage_attachments -> active_storage_blobs (blob_id));
+joinable!(active_storage_variant_records -> active_storage_blobs (blob_id));
+joinable!(segments -> variants (variant_id));
+joinable!(signature_keys -> users (user_id));
+joinable!(variants -> videos (video_id));
+joinable!(video_watch_events -> users (user_id));
+joinable!(video_watch_events -> videos (video_id));
+joinable!(videos -> users (user_id));
+joinable!(webhooks -> users (user_id));
+
+allow_tables_to_appear_in_same_query!(
+    access_tokens,
+    active_storage_attachments,
+    active_storage_blobs,
+    active_storage_variant_records,
+    ar_internal_metadata,
+    schema_migrations,
+    segments,
+    signature_keys,
+    users,
+    variants,
+    video_watch_events,
+    videos,
+    webhooks,
+);
